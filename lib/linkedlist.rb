2331dbaa188ff3d26e034c8fe5f6868e7864944c
JSE955 <jordanscottedwards@gmail.com>
2024-06-17 12:05:36 -0400

    Build append method

--- lib/linkedlist.rb
+++ lib/linkedlist.rb
@@ -6,4 +6,19 @@ class LinkedList
   def initialize
     @head = nil
   end
+
+  def append(value)
+    new_node = Node.new
+    new_node.value = value
+    if head.nil?
+      temp = new_node
+      temp.next_node = nil
+      self.head = temp
+    else
+      temp = head
+      temp = temp.next_node until temp.next_node.nil?
+      new_node.next_node = nil
+      temp.next_node = new_node
+    end
+  end
 end
---
+++ main.rb
@@ -0,0 +1,7 @@
+require_relative 'lib/linkedlist'
+
+test_list = LinkedList.new
+test_list.append(100)
+test_list.append(200)
+test_list.append(300)
+p test_list
